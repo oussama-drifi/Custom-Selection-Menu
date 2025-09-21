7d478ba2d0d5db3f0f6753d35f2dd8301cffdc3a
oussamadrf <oussamadrifi107@gmail.com>
2025-09-21 23:41:15 +0100

    saved

--- SelectMenu/selectmenu.js
+++ SelectMenu/selectmenu.js
@@ -12,7 +12,7 @@ items.forEach(item => {
 const input = document.getElementById("input");
 const fieledContainer = document.querySelector(".fieled-container");
 const options = document.querySelectorAll(".options > div");
-const icon = document.querySelector(".selected-option span i");
+const iconHolder = document.querySelector(".selected-option span:last-child");
 const selectedOption = document.querySelector(".selected-option");
 const displayedOption = document.querySelector(".selected-option .selected");
 
@@ -22,7 +22,7 @@ fieledContainer.addEventListener("click", event => {
     // if the select zone is clicked
     if(selectedOption.contains(event.target)){
         optionsContainer.classList.toggle("show-options");
-        icon.classList.toggle("rotate");
+        iconHolder.classList.toggle("rotate");
     }
 });
 // when selecting an option
@@ -31,7 +31,7 @@ options.forEach(option => {
         // hide the menu
         optionsContainer.classList.remove("show-options");
         // return the icon to initial state
-        icon.classList.remove("rotate");
+        iconHolder.classList.remove("rotate");
         // update the area shown input value
         displayedOption.textContent = option.textContent;
         // update the hidden input value
@@ -44,6 +44,6 @@ options.forEach(option => {
 document.addEventListener("click", event => {
     if(!selectedOption.contains(event.target)){
         optionsContainer.classList.remove("show-options");
-        icon.classList.remove("rotate");
+        iconHolder.classList.remove("rotate");
     }
 });
